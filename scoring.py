02173c3ceb6716fc8e08d679ae4e35243e24e1d4
zrosen-wizard <zachary.rosen@wizard.com>
2022-07-29 12:03:30 -0700

    first commit

--- scoring.py
+++ scoring.py
@@ -11,16 +11,24 @@ class scoring():
         return self.df[col]
 
     def fj(self, pix_col, fJ_col):
+
         M = (~self.df[fJ_col].isna().values).astype(float)
         I = self.df[pix_col].values.reshape(-1,1)
 
         k = (I * M).sum(axis=0)
-        Y = ((k - I) * M) / (M.sum(axis=0)-1)
+        N = (M.sum(axis=0)-1)
+
+        Y = ((k - I) * M) / N
         Y = np.nan_to_num(Y, posinf=0.,neginf=0.)
-        Y = (Y == 0).astype(float) + Y
+        Y = (M == 0).astype(float) + Y
 
         return np.prod(Y, axis=-1)
 
+    def data(self,col=None):
+        if bool(col):
+            return self.df[col]
+        else:
+            return self.df
 
     def get_cols(self):
         return self.df.columns.values
